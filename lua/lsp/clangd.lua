b330a0f44548f69aa0386ba66a4af425e3967a18
Hanchin Hsieh <me@yuchanns.xyz>
2024-12-06 12:20:16 +0800

    lsp: add clangd

---
+++ lua/lsp/clangd.lua
@@ -0,0 +1,12 @@
+local system = require("utils.system")
+
+if not system.is_executable("clangd") then
+  return
+end
+
+local lspconfig = require("lspconfig")
+
+lspconfig["clangd"].setup({
+  cmd = { "clangd", "--offset-encoding=utf-16" },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+})
