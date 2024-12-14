44b8357d745456a39c1e707cbe6b153a3822c0bd
Hanchin Hsieh <me@yuchanns.xyz>
2024-12-14 17:57:26 +0800

    lsp: move setup
    Signed-off-by: Hanchin Hsieh <me@yuchanns.xyz>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,128 +4,13 @@ local cmd = keymap.cmd
 local silent, noremap = keymap.silent, keymap.noremap
 local opts = keymap.new_opts
 local autocmd = require("utils.autocmd")
-local Util = require("lazy.core.util")
-
-local function setup_cmp()
-  -- To use ultisnips, you need to have python installed, and two modules are required:
-  -- * pynvim
-  -- * typing_extensions
-  -- Make sure python is available in your $PATH.
-  -- pyenv is recommendation.
-  -- You can get the executable path by `pyenv which python` and then set it to the $PATH.
-  local cmp = require("cmp")
-  local lspkind = require("lspkind")
-  cmp.setup({
-    formatting = {
-      format = lspkind.cmp_format({
-        mode = "symbol",       -- show only symbol annotations
-        maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-        ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-        -- The function below will be called before any actual modifications from lspkind
-        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-        before = function(_, vim_item)
-          return vim_item
-        end,
-      }),
-    },
-    experimental = {
-      ghost_text = true,
-    },
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      end,
-    },
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "ultisnips" }, -- For ultisnips users.
-      { name = "nvim_lua" },
-      { name = "buffer" },
-      { name = "path" },
-      { name = "dictionary" },
-      { name = "orgmode" },
-      {
-        name = "spell",
-        option = {
-          keep_all_entries = false,
-          enable_in_context = function()
-            return true
-          end,
-        },
-      },
-    }),
-    mapping = {
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-      ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-    },
-  })
-  cmp.setup.cmdline({ "/", "?" }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = "buffer" },
-    },
-  })
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path" },
-    }, {
-      { name = "cmdline" },
-    }),
-  })
-end
-
-local function setup_treesitter()
-  vim.api.nvim_command("set foldmethod=expr")
-  vim.api.nvim_command("set foldexpr=nvim_treesitter#foldexpr()")
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = { "markdown", "markdown_inline", "vimdoc", "go", "typescript", "rust", "c", "vim", "lua", "query" },
-    ignore_install = { "phpdoc" },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { "org" },
-      disable = function(lang, bufnr)
-        return lang == "javascript" and vim.api.nvim_buf_line_count(bufnr) > 1000
-      end,
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        },
-      },
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn", -- set to `false` to disable one of the mappings
-        node_incremental = ".",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
-    },
-  })
-end
+local loader = require("utils.loader")
 
 autocmd.lsp_attach(function(client, bufnr)
   require("illuminate").on_attach(client, bufnr)
 end)
 
-autocmd.user_pattern("VeryLazy", function()
-  -- auto load language-server
-  Util.lsmod("lsp", function(modname, _)
-    require(modname)
-  end)
-  setup_treesitter()
-  setup_cmp()
-end)
+autocmd.user_pattern("VeryLazy", loader.callback_load_mods({ "lsp", "lsp.setup" }))
 
 autocmd.user_cmd("LspRestartHint", function()
   vim.cmd("LspRestart")
@@ -169,7 +54,7 @@ nmap({
   { "]e",        cmd("Lspsaga diagnostic_jump_next"),                           opts(noremap, silent) },
   { "<Leader>e", cmd("Lspsaga show_line_diagnostics"),                          opts(noremap, silent) },
   { "<Leader>a", cmd("Lspsaga outline"),                                        opts(noremap, silent) },
-  { "<Leader>r", cmd("LspRestartHint"),                                             opts(noremap, silent) },
+  { "<Leader>r", cmd("LspRestartHint"),                                         opts(noremap, silent) },
 })
 
 
