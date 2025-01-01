bdfc26c1b9de1e8f4d5f32ccfbe23a13328bae6c
Hanchin Hsieh <me@yuchanns.xyz>
2025-01-01 21:04:31 +0800

    leetcode: temp use my branch

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,6 +1,7 @@
 return {
-  "kawre/leetcode.nvim",
+  "yuchanns/leetcode.nvim",
   build = ":TSUpdate html",
+  branch = "top-150",
   dependencies = {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim",
